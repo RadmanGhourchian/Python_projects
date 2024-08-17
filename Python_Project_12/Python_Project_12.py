df88ae5d3971a6ab7f0e123a273584b918cf2134
RadmanGhourchian <radmanssd@outlook.com>
2024-08-17 14:54:35 +0330

    A little bit of starter code.

--- Python_Project_12/Python_Project_12.py
+++ Python_Project_12/Python_Project_12.py
@@ -5,6 +5,6 @@ win.title("Python Project 12")
 win.geometry("500x500")
 win.resizable(False, False)
 
-tk.Label(win, text="Python Project 12", fg="light green", font="Arail").place(x=100, y=130)
+tk.Label(win, text="Python Project 12", fg="dark green", font="Arail").place(x=100, y=130)
 
 win.mainloop()
