d3b7a34df6d6bde98d007cd0efbbbffc8fd610d5
RadmanGhourchian <radmanssd@outlook.com>
2024-08-17 14:44:21 +0330

    A little bit of starter code.

--- Python_Project_12/Python_Project_12.py
+++ Python_Project_12/Python_Project_12.py
@@ -1,1 +1,10 @@
-pass
+import tkinter as tk
+
+win = tk.Tk()
+win.title("Python Project 12")
+win.geometry("500x500")
+win.resizable(False, False)
+
+tk.Label(win, text="Python Project 12", fg="light green", font="Arail").place(x=100, y=130)
+
+win.mainloop()
